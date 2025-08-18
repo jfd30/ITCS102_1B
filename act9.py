28d6753f1af4208631824aef0c4501bab4f09b2d
Justin Deodores <165528347+jfd30@users.noreply.github.com>
2025-08-18 15:49:33 +0800

    Add files via upload

---
+++ act9.py
@@ -0,0 +1,11 @@
+a = 5
+b = 5
+
+c = 6
+d = 5
+username = 'justin'
+password = 'jfd123'
+print((username == 'justin') and (password == 'jfd123' ))
+#print( (a > b) and (c < d ) )
+#print( (a > b) or (c < d) )
+print(not((a > b) or (c < d)))
