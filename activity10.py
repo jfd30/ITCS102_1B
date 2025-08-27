34e63b6fb75206f881c39e8b29a7fab82f670edc
Justin Deodores <165528347+jfd30@users.noreply.github.com>
2025-08-27 15:11:25 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,10 @@
+username = "jfd"
+password = "jfd123"
+
+uname = input("Enter username --> ")
+pword = input("Enter password --> ")
+
+if (uname == username) and (pword == password) :
+    print("acces granted")
+else:
+    print("access denied")
