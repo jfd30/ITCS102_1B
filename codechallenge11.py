085f8cca296b866ae0db6be9efc613e269156a8b
Justin Deodores <165528347+jfd30@users.noreply.github.com>
2025-10-25 22:04:17 +0800

    Create codechallenge11.py

---
+++ codechallenge11.py
@@ -0,0 +1,8 @@
+for x in range(1,11,1):
+    for y in range(10,x,-1):
+        print(' ', end='')
+    for i in range(1,x,1):
+        print('x', end='')
+    for k in range(1,x,1):
+        print('x', end='')
+    print()
