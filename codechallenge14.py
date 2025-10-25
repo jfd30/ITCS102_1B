9572dc2a12775925b5a7281e19f0433b8df347cb
Justin Deodores <165528347+jfd30@users.noreply.github.com>
2025-10-25 22:11:44 +0800

    Create codechallenge14.py

---
+++ codechallenge14.py
@@ -0,0 +1,15 @@
+list = []
+
+print("Pick Your Anime List")
+while True:
+    anime = input("Enter Anime Title (Type 'exit' to stop): ")
+    if anime.lower()== 'exit':
+        print("End")
+        break
+    list.append(anime)
+
+num = len(list)
+print("Your Anime List Contains:")
+for i in range(1,num+1,1):
+    print(f'- {list[i-1]}')
+print("You know ball dawg")
