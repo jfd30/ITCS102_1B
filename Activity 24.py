095a9eefd1bfa8dac0df7ea4f95b4c5c2744d2e9
Justin Deodores <165528347+jfd30@users.noreply.github.com>
2025-12-28 13:34:55 +0800

    Create Activity 24.py

---
+++ Activity 24.py
@@ -0,0 +1,9 @@
+from activity24_1 import GreetPerson, GreetInfo
+
+def Welcome():  
+    print("Hey there, welcome to my function")
+    print("Please browse around")
+
+Welcome()            
+GreetPerson('jay')     
+GreetInfo('jay', 'lucena', '19')
