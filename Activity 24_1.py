4896e462a712b862a63327b7c702e1b35af96e81
Justin Deodores <165528347+jfd30@users.noreply.github.com>
2025-12-28 13:35:33 +0800

    Create Activity 24_1.py

---
+++ Activity 24_1.py
@@ -0,0 +1,7 @@
+def GreetPerson(name):
+    print(f"Hi, {name}. What are you up to? Hope you're having a good day!")
+
+def GreetInfo(name, loc, age):
+    print(f"Hi, {name} from {loc} - {age} yrs old")
+
+
