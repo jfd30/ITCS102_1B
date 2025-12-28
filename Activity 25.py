14c3e251b42e43902ac0f2c5fa0f7efec16ec867
Justin Deodores <165528347+jfd30@users.noreply.github.com>
2025-12-28 13:38:38 +0800

    Create Activity 25.py

---
+++ Activity 25.py
@@ -0,0 +1,15 @@
+# Dictionary
+
+Dogs = ['Aspin', 'Husky', 'Bulldog', 'Chihuahua', 'Pitbull']
+
+print(dogs[3])
+
+dog_traits = {'color': 'White', 'breed': 'Aspin', 'size': 'Small', 'behavior': 'Playful', 'fur': 'Fluffy', 'eyes': 'Blue'}
+print(dog_traits['fur'])
+
+dog_traits['smell'] = 'Foul'
+print(dog_traits)
+
+dog_traits.pop('size')
+print(dog_traits)
+
